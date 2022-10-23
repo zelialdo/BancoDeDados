ed686896a8fd8434eb04c03e7b54965c29a48f79
Zelialdo Sueldo da Silva Lima <71799490+zelialdo@users.noreply.github.com>
2022-10-22 22:01:49 -0300

    Create inserirPrefeitos.sql

---
+++ inserirPrefeitos.sql
@@ -0,0 +1,20 @@
+SELECT * FROM cidades;
+
+INSERT INTO  prefeitos 
+    (nome, cidade_id)
+VALUES
+    ('Rodrigo Neves', 4),
+    ('Raquel Lira', 2),
+    ('Zenaldo Coutinho', null);
+
+SELECT * FROM prefeitos;
+
+INSERT INTO prefeitos 
+    (nome, cidade_id) 
+VALUES
+    ('Rafael Greca', null);
+
+INSERT INTO prefeitos 
+    (nome, cidade_id) 
+VALUES
+    ('Rodrigo Pinheiro', 3);
